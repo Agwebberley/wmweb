cf70f4533fd0b88ab7fe9f0e6997798a66425e47
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2021-04-09 09:36:39 -0700

    sample

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById("popupbutton").addEventListener("click", myFunction);
+
+function myFunction(){
+  let params = `scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,
+width=1024,height=1024,left=0,top=0`;
+
+  window.open("https://webbmath.com/sample.html", "WebbMath", params);
+}
